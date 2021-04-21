e126e121ec4439b98bd716febd4c7fc12f79e8a8
Rodrigo <https://github.com/Rodrigo-des/Desenvolvimento-de-testes-unitarios-para-validar-uma-API-REST-de-gerenciamento-estoques-de-cerveja.git>
2021-04-21 15:24:51 -0300

    Adicionado classe Conta

---
+++ src/main/kotlin/one/digitalInnovation/digiOneBank/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalInnovation.digiOneBank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val numeroConta: String,
+    val saldo: BigDecimal
+    ) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
--- src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digiOneBank/Pessoa.kt
@@ -5,6 +5,8 @@ class Pessoa {
     var cpf:String = "123.456.789.12"
     private set
 
+    constructor()
+
     fun pessoaInfo() = "Cliente: $nome, com CPF: $cpf "
 
 }
